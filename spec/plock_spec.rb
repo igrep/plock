8d2d363933359881fbef8d90c62ad412272afe98
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-04-21 17:58:19 +0900

    change the spec to return a pair of the proc source and the result

--- spec/plock_spec.rb
+++ spec/plock_spec.rb
@@ -3,7 +3,7 @@ require 'plock'
 describe Plock do
   describe '.inspect ' do
     subject { described_class.inspect_block { 1 + 1 } }
-    it { should eq '(1 + 1) #=> 2' }
+    it { should eq ['(1 + 1)', 2] }
   end
   describe '#p' do
     subject { p { 1 + 1 } }
