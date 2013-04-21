d142a9a2c4caa8b7b8d387e6f25f9c387e040844
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-04-21 16:54:13 +0900

    add rspec

---
+++ spec/plock_spec.rb
@@ -0,0 +1,16 @@
+require 'plock'
+
+describe Plock do
+  describe '.inspect ' do
+    subject { described_class.inspect { 1 + 1 } }
+    it { should eq '1 + 1 #=> 2' }
+  end
+  describe '#p' do
+    subject { p { 1 + 1 } }
+    it { should be 2 }
+    it 'calls Plock.inspect' do
+      Plock.should_receive :inspect
+      p { 1 + 1 }
+    end
+  end
+end
