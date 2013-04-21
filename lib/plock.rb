ce839b709190ce41c4a796477920fda8895ed63b
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-04-21 17:22:22 +0900

    pass the spec by changing both implementation and spec

--- lib/plock.rb
+++ lib/plock.rb
@@ -4,6 +4,6 @@ require "plock/version"
 module Plock
   def self.inspect_block &block
     result = block.call
-    "#{ block.to_raw_source( attached_to: __method__ ) } #=> #{result.inspect}"
+    "#{ block.to_source( attached_to: __method__, strip_enclosure: true ) } #=> #{result.inspect}"
   end
 end
