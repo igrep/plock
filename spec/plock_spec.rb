8cb19e82e0ed6165d5374cd774a148638f0a178e
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-04-29 18:12:15 +0900

    fix incorrect use of context

--- spec/plock_spec.rb
+++ spec/plock_spec.rb
@@ -10,13 +10,14 @@ describe Plock do
     it { should eq ['(a + 1)', 2] }
   end
   describe '.format' do
-    context 'Plock.output_format = "%b #=> %r"'
-    subject { described_class.format '(a + 1)', 2 }
-    it { should eq '(a + 1) #=> 2' }
-    it 'never changes the output_format desructively' do
-      described_class.output_format = '%b and %r SHOULD NOT BE CHANGED'
-      described_class.format 'block', 'result'
-      described_class.output_format.should eq '%b and %r SHOULD NOT BE CHANGED'
+    context 'when Plock.output_format = "%b #=> %r"' do
+      subject { described_class.format '(a + 1)', 2 }
+      it { should eq '(a + 1) #=> 2' }
+      it 'never changes the output_format desructively' do
+        described_class.output_format = '%b and %r SHOULD NOT BE CHANGED'
+        described_class.format 'block', 'result'
+        described_class.output_format.should eq '%b and %r SHOULD NOT BE CHANGED'
+      end
     end
   end
 end
